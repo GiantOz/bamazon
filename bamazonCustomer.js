fb5a373a9b33ecca378e708028a51db8e8a70180
Giant Oz <46080570+GiantOz@users.noreply.github.com>
2019-07-17 23:35:16 -0400

    Add files via upload

---
+++ bamazonCustomer.js
@@ -0,0 +1,146 @@
+[
+{
+"customerName": "Saima",
+"customerEmail": "saima@gmail.com",
+"phoneNumber": "979-587-0887",
+"customerID": "saimacool"
+},
+{
+"customerName": "Patrick Follett",
+"phoneNumber": "14122603991",
+"customerEmail": "pwf592110@gmail.com",
+"customerID": "Pwilly"
+},
+{
+"customerName": "Pete",
+"phoneNumber": "555-1212",
+"customerEmail": "s********@yahoo.com",
+"customerID": "6434"
+},
+{
+"customerName": "Pinar",
+"phoneNumber": "1234567890",
+"customerEmail": "pinar@gmail.com",
+"customerID": "12345"
+},
+{
+"customerName": "Toby",
+"phoneNumber": "9082988000",
+"customerEmail": "tobyajackson@gmail.com",
+"customerID": "tobyaj1234"
+},
+{
+"customerName": "Romeo Kilo",
+"phoneNumber": "5558675309",
+"customerEmail": "me@somewhereelse.com",
+"customerID": "romeokilo125"
+},
+{
+"customerName": "Pam",
+"phoneNumber": "555-555",
+"customerEmail": "something@somewhere.com",
+"customerID": "13"
+},
+{
+"customerName": "Jeff Fah Fah",
+"phoneNumber": "215-555-5555",
+"customerEmail": "uPENNRox@gmail.com",
+"customerID": "LPS BootCamp"
+},
+{
+"customerName": "Andrew Weber",
+"phoneNumber": "6108062858",
+"customerEmail": "ajweber102@gmail.com",
+"customerID": "ajweber102"
+},
+{
+"customerName": "Aaron MacInnis",
+"phoneNumber": "000-000-0000",
+"customerEmail": "ironam1@gmail.com",
+"customerID": "ironam1"
+},
+{
+"customerName": "Bruce",
+"phoneNumber": "203-555-5555",
+"customerEmail": "bruce@lps.com",
+"customerID": "123321"
+},
+{
+"customerName": "Temi Olaoluniyi",
+"phoneNumber": "2674755147",
+"customerEmail": "tolaoluniyi@gmail.com",
+"customerID": "714832"
+},
+{
+"customerName": "Sherard Graham",
+"phoneNumber": "6106592804",
+"customerEmail": "grahamgraphics@aol.com",
+"customerID": ""
+},
+{
+"customerName": "Tsarina Islam",
+"phoneNumber": "9999999999",
+"customerEmail": "otakuchick95@gmail.com",
+"customerID": ""
+},
+{
+"customerName": "Yukie",
+"phoneNumber": "6891012576",
+"customerEmail": "me@me.com",
+"customerID": "Yutsukushi"
+},
+{
+"customerName": "Gayah Karmo",
+"phoneNumber": "111-111-1112",
+"customerEmail": "gman@gmoney.com",
+"customerID": "gkarmo"
+},
+{
+"customerName": "guest1",
+"phoneNumber": "555-555-3462",
+"customerEmail": "this@email.com",
+"customerID": "guest31421421"
+},
+{
+"customerName": "Bryant",
+"phoneNumber": "610-680-7670",
+"customerEmail": "giantoz29@gmail.com",
+"customerID": "001"
+},
+{
+"customerName": "Shawn",
+"phoneNumber": "610-446-1029",
+"customerEmail": "shawnluther99@gmail.com",
+"customerID": "98"
+},
+{
+"customerName": "Pinar",
+"phoneNumber": "654234565",
+"customerEmail": "one@gmail.com",
+"customerID": ""
+},
+{
+"customerName": "zach",
+"phoneNumber": "123-456-7890",
+"customerEmail": "zfdunn@comcast.net",
+"customerID": "zfdunn"
+},
+{
+"customerName": "Robert Turner",
+"phoneNumber": "2234454554",
+"customerEmail": "rturner1298@gmail.com",
+"customerID": "345523"
+},
+{
+"customerName": "Alexis",
+"phoneNumber": "2672673268",
+"customerEmail": "arajis06@gmail.com",
+"customerID": "arajis"
+},
+{
+"customerName": "Karen",
+"phoneNumber": "555-1212",
+"customerEmail": "my@email.com",
+"customerID": "karensres"
+}
+]
