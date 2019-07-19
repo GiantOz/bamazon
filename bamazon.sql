a3783b6384c0ff6b0e811e66413d0afd8c1ca293
Giant Oz <46080570+GiantOz@users.noreply.github.com>
2019-07-19 08:20:05 -0400

    Add files via upload

--- bamazon.sql
+++ bamazon.sql
@@ -4,11 +4,50 @@ CREATE DATABASE bamazonDB;
 
 USE bamazonDB;
 
-CREATE TABLE Products (
-  item_id INT NOT NULL AUTO_INCREMENT,
-  product_name VARCHAR(45) NULL,
-  department_name DECIMAL(10,2) NULL,
-  price VARCHAR(10) NULL,
-  stock_quantity INT NULL,
+CREATE TABLE Products(
+  id INT NOT NULL AUTO_INCREMENT,
+  item VARCHAR(45) NULL,
+  department VARCHAR(45) NULL,
+  price DECIMAL(10,2) NULL,
+  quantity INT NULL,
   PRIMARY KEY (id)
 );
+
+INSERT INTO Products (item)
+VALUES ("Computer");
+
+INSERT INTO Products (item)
+VALUES ("Printer");
+
+INSERT INTO Products (item)
+VALUES ("Computer Monitor");
+
+INSERT INTO Products (item)
+VALUES ("Desk");
+
+INSERT INTO Products (item)
+VALUES ("Chair");
+
+INSERT INTO Products (item)
+VALUES ("IPad");
+
+INSERT INTO Products (item)
+VALUES ("IPhone");
+
+INSERT INTO Products (item)
+VALUES ("Desk Lamp");
+
+INSERT INTO Products (item)
+VALUES ("Computer Speakers");
+
+INSERT INTO Products (item)
+VALUES ("Headset");
+
+INSERT INTO Products (item)
+VALUES ("Phone Charger");
+
+INSERT INTO Products (item)
+VALUES ("Desk Globe");
+
+INSERT INTO Products (item)
+VALUES ("Antique Letter Opener");
