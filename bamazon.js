4eea8c0e8906fcd459e8cd93b32f643a8aa35c37
Giant Oz <46080570+GiantOz@users.noreply.github.com>
2019-07-19 08:49:51 -0400

    Add files via upload

---
+++ bamazon.js
@@ -0,0 +1,92 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazonDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  createProduct();
+});
+
+function createProduct() {
+  console.log("Inserting a new item...\n");
+  var query = connection.query(
+    "INSERT INTO products SET ?",
+    {
+      product: "Computer",
+      price: 3.0,
+      quantity: 50
+    },
+    function(err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " item inserted!\n");
+      // Call updateProduct AFTER the INSERT completes
+      updateProduct();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
+
+function updateProduct() {
+  console.log("Updating all item A quantities...\n");
+  var query = connection.query(
+    "UPDATE products SET ? WHERE ?",
+    [
+      {
+        quantity: 100
+      },
+      {
+        product: "item A"
+      }
+    ],
+    function(err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " products updated!\n");
+      // Call deleteProduct AFTER the UPDATE completes
+      deleteProduct();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
+
+function deleteProduct() {
+  console.log("Deleting all item B...\n");
+  connection.query(
+    "DELETE FROM products WHERE ?",
+    {
+      product: "item B"
+    },
+    function(err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " products deleted!\n");
+      // Call readProducts AFTER the DELETE completes
+      readProducts();
+    }
+  );
+}
+
+function readProducts() {
+  console.log("Selecting all items...\n");
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log(res);
+    connection.end();
+  });
+}
